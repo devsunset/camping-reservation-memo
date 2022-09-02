aa77306473e876692b52617be18a23e059dbe3a3
devsunset <devsunset@gmail.com>
2022-09-03 07:49:12 +0900

    dev

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -7,38 +7,37 @@ const UserSchema = new Schema({
   hashedPassword: String,
 });
 
-UserSchema.methods.setPassword = async function(password) {
+UserSchema.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
 };
 
-UserSchema.methods.checkPassword = async function(password) {
+UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
-  return result; // true / false
+  return result;
 };
 
-UserSchema.methods.serialize = function() {
+UserSchema.methods.serialize = function () {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
 };
 
-UserSchema.methods.generateToken = function() {
+UserSchema.methods.generateToken = function () {
   const token = jwt.sign(
-    // 첫번째 파라미터엔 토큰 안에 집어넣고 싶은 데이터를 넣습니다
     {
       _id: this.id,
       username: this.username,
     },
-    process.env.JWT_SECRET, // 두번째 파라미터에는 JWT 암호를 넣습니다
+    process.env.JWT_SECRET,
     {
-      expiresIn: '7d', // 7일동안 유효함
+      expiresIn: '7d',
     },
   );
   return token;
 };
 
-UserSchema.statics.findByUsername = function(username) {
+UserSchema.statics.findByUsername = function (username) {
   return this.findOne({ username });
 };
 
