c850b0b895c6dfcf20238da993ee4d93c26a0bb5
devsunset <devsunset@gmail.com>
2022-09-02 15:31:30 +0900

    dev

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -32,14 +32,13 @@ const sanitizeOption = {
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
-    ctx.status = 400; // Bad Request
+    ctx.status = 400;
     return;
   }
   try {
     const post = await Post.findById(id);
-    // 포스트가 존재하지 않을 때
     if (!post) {
-      ctx.status = 404; // Not Found
+      ctx.status = 404;
       return;
     }
     ctx.state.post = post;
@@ -58,26 +57,16 @@ export const checkOwnPost = (ctx, next) => {
   return next();
 };
 
-/*
-  POST /api/posts
-  {
-    title: '제목',
-    body: '내용',
-    tags: ['태그1', '태그2']
-  }
-*/
 export const write = async (ctx) => {
   const schema = Joi.object().keys({
-    // 객체가 다음 필드를 가지고 있음을 검증
-    title: Joi.string().required(), // required() 가 있으면 필수 항목
+    title: Joi.string().required(),
     body: Joi.string().required(),
-    tags: Joi.array().items(Joi.string()).required(), // 문자열로 이루어진 배열
+    tags: Joi.array().items(Joi.string()).required(),
   });
 
-  // 검증 후, 검증 실패시 에러처리
   const result = schema.validate(ctx.request.body);
   if (result.error) {
-    ctx.status = 400; // Bad Request
+    ctx.status = 400;
     ctx.body = result.error;
     return;
   }
@@ -97,7 +86,6 @@ export const write = async (ctx) => {
   }
 };
 
-// html 을 없애고 내용이 너무 길으면 200자로 제한시키는 함수
 const removeHtmlAndShorten = (body) => {
   const filtered = sanitizeHtml(body, {
     allowedTags: [],
@@ -105,12 +93,7 @@ const removeHtmlAndShorten = (body) => {
   return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
 };
 
-/*
-  GET /api/posts?username=&tag=&page=
-*/
 export const list = async (ctx) => {
-  // query 는 문자열이기 때문에 숫자로 변환해주어야합니다.
-  // 값이 주어지지 않았다면 1 을 기본으로 사용합니다.
   const page = parseInt(ctx.query.page || '1', 10);
 
   if (page < 1) {
@@ -119,7 +102,6 @@ export const list = async (ctx) => {
   }
 
   const { tag, username } = ctx.query;
-  // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
   const query = {
     ...(username ? { 'user.username': username } : {}),
     ...(tag ? { tags: tag } : {}),
@@ -143,60 +125,42 @@ export const list = async (ctx) => {
   }
 };
 
-/*
-  GET /api/posts/:id
-*/
 export const read = async (ctx) => {
   ctx.body = ctx.state.post;
 };
 
-/*
-  DELETE /api/posts/:id
-*/
 export const remove = async (ctx) => {
   const { id } = ctx.params;
   try {
     await Post.findByIdAndRemove(id).exec();
-    ctx.status = 204; // No Content (성공은 했지만 응답할 데이터는 없음)
+    ctx.status = 204;
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
-/*
-  PATCH /api/posts/:id
-  {
-    title: '수정',
-    body: '수정 내용',
-    tags: ['수정', '태그']
-  }
-*/
 export const update = async (ctx) => {
   const { id } = ctx.params;
-  // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
   const schema = Joi.object().keys({
     title: Joi.string(),
     body: Joi.string(),
     tags: Joi.array().items(Joi.string()),
   });
 
-  // 검증 후, 검증 실패시 에러처리
   const result = schema.validate(ctx.request.body);
   if (result.error) {
-    ctx.status = 400; // Bad Request
+    ctx.status = 400;
     ctx.body = result.error;
     return;
   }
 
-  const nextData = { ...ctx.request.body }; // 객체를 복사하고
-  // body 값이 주어졌으면 HTML 필터링
+  const nextData = { ...ctx.request.body };
   if (nextData.body) {
     nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
   }
   try {
     const post = await Post.findByIdAndUpdate(id, nextData, {
-      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
-      // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
+      new: true,
     }).exec();
     if (!post) {
       ctx.status = 404;
--- backend/src/main.js
+++ backend/src/main.js
@@ -10,7 +10,6 @@ import send from 'koa-send';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
 
-// 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
@@ -25,27 +24,20 @@ mongoose
 const app = new Koa();
 const router = new Router();
 
-// 라우터 설정
-router.use('/api', api.routes()); // api 라우트 적용
-
-// 라우터 적용 전에 bodyParser 적용
+router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 
-// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 const buildDirectory = path.resolve(__dirname, '../../frontend/build');
 app.use(serve(buildDirectory));
 app.use(async (ctx) => {
-  // Not Found 이고, 주소가 /api 로 시작하지 않는 경우
   if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
-    // index.html 내용을 반환
     await send(ctx, 'index.html', { root: buildDirectory });
   }
 });
 
-// PORT 가 지정되어있지 않다면 4000 을 사용
 const port = PORT || 4000;
 app.listen(port, () => {
   console.log('Listening to port %d', port);
