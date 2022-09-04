056f29ade71db58a17ee0d26d394edaceaa4082b
devsunset <devsunset@gmail.com>
2022-09-04 16:18:38 +0900

    dev

--- frontend/src/components/posts/PostList.js
+++ frontend/src/components/posts/PostList.js
@@ -69,7 +69,105 @@ const PostList = ({ posts, loading, error, showWriteButton }) => {
 
   return (
     <PostListBlock>
-      <Calendar usageStatistics={false} />
+      <Calendar
+        height="300px"
+        calendars={[
+          {
+            id: '0',
+            name: 'Private',
+            bgColor: '#9e5fff',
+            borderColor: '#9e5fff',
+          },
+          {
+            id: '1',
+            name: 'Company',
+            bgColor: '#00a9ff',
+            borderColor: '#00a9ff',
+          },
+        ]}
+        disableDblClick={true}
+        disableClick={false}
+        isReadOnly={false}
+        month={{
+          startDayOfWeek: 0,
+        }}
+        schedules={[
+          {
+            id: '1',
+            calendarId: '0',
+            title: 'TOAST UI Calendar Study',
+            category: 'time',
+            dueDateClass: '',
+            start: '2022-09-28T12:00:00',
+            end: '2022-09-28T13:30:00',
+          },
+          {
+            id: '2',
+            calendarId: '0',
+            title: 'Practice',
+            category: 'milestone',
+            dueDateClass: '',
+            start: '2022-09-28T12:00:00',
+            end: '2022-09-28T13:30:00',
+            isReadOnly: true,
+          },
+          {
+            id: '3',
+            calendarId: '0',
+            title: 'FE Workshop',
+            category: 'allday',
+            dueDateClass: '',
+            start: '2022-09-28T12:00:00',
+            end: '2022-09-28T13:30:00',
+            isReadOnly: true,
+          },
+          {
+            id: '4',
+            calendarId: '0',
+            title: 'Report',
+            category: 'time',
+            dueDateClass: '',
+            start: '2022-09-28T12:00:00',
+            end: '2022-09-28T13:30:00',
+          },
+        ]}
+        scheduleView
+        taskView
+        template={{
+          milestone(schedule) {
+            return `<span style="color:#fff;background-color: ${schedule.bgColor};">${schedule.title}</span>`;
+          },
+          milestoneTitle() {
+            return 'Milestone';
+          },
+          allday(schedule) {
+            return `${schedule.title}<i class="fa fa-refresh"></i>`;
+          },
+          alldayTitle() {
+            return 'All Day';
+          },
+        }}
+        // theme={myTheme}
+        timezones={[
+          {
+            timezoneOffset: 540,
+            displayLabel: 'GMT+09:00',
+            tooltip: 'Seoul',
+          },
+          {
+            timezoneOffset: -420,
+            displayLabel: 'GMT-08:00',
+            tooltip: 'Los Angeles',
+          },
+        ]}
+        useDetailPopup
+        useCreationPopup
+        view={'month'} // You can also set the `defaultView` option.
+        week={{
+          showTimezoneCollapseButton: true,
+          timezonesCollapsed: true,
+        }}
+      />
       <WritePostButtonWrapper>
         {showWriteButton && (
           <Button cyan to="/write">
