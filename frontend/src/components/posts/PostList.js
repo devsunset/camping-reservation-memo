f7385cbc79c9e6aa6dca7963630095673724acc1
devsunset <devsunset@gmail.com>
2022-09-04 17:19:06 +0900

    dev

--- frontend/src/components/posts/PostList.js
+++ frontend/src/components/posts/PostList.js
@@ -6,8 +6,9 @@ import palette from '../../lib/styles/palette';
 import SubInfo from '../common/SubInfo';
 import Tags from '../common/Tags';
 import { Link } from 'react-router-dom';
-import Calendar from '@toast-ui/react-calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const PostListBlock = styled(Responsive)`
   margin-top: 3rem;
@@ -67,106 +68,33 @@ const PostList = ({ posts, loading, error, showWriteButton }) => {
     return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
   }
 
+  moment.locale('ko-KR');
+  const localizer = momentLocalizer(moment);
+
+  const reservation_day = [
+    {
+      title: '천왕산 캠핑장',
+      allDay: true,
+      start: new Date(2022, 8, 9),
+      end: new Date(2022, 8, 9),
+    },
+    {
+      title: '도덕산 캠핑장',
+      allDay: true,
+      start: new Date(2022, 8, 9),
+      end: new Date(2022, 8, 9),
+    },
+  ];
+
   return (
     <PostListBlock>
       <Calendar
-        height="300px"
-        calendars={[
-          {
-            id: '0',
-            name: 'Private',
-            bgColor: '#9e5fff',
-            borderColor: '#9e5fff',
-          },
-          {
-            id: '1',
-            name: 'Company',
-            bgColor: '#00a9ff',
-            borderColor: '#00a9ff',
-          },
-        ]}
-        disableDblClick={true}
-        disableClick={false}
-        isReadOnly={false}
-        month={{
-          startDayOfWeek: 0,
-        }}
-        schedules={[
-          {
-            id: '1',
-            calendarId: '0',
-            title: 'TOAST UI Calendar Study',
-            category: 'time',
-            dueDateClass: '',
-            start: '2022-09-28T12:00:00',
-            end: '2022-09-28T13:30:00',
-          },
-          {
-            id: '2',
-            calendarId: '0',
-            title: 'Practice',
-            category: 'milestone',
-            dueDateClass: '',
-            start: '2022-09-28T12:00:00',
-            end: '2022-09-28T13:30:00',
-            isReadOnly: true,
-          },
-          {
-            id: '3',
-            calendarId: '0',
-            title: 'FE Workshop',
-            category: 'allday',
-            dueDateClass: '',
-            start: '2022-09-28T12:00:00',
-            end: '2022-09-28T13:30:00',
-            isReadOnly: true,
-          },
-          {
-            id: '4',
-            calendarId: '0',
-            title: 'Report',
-            category: 'time',
-            dueDateClass: '',
-            start: '2022-09-28T12:00:00',
-            end: '2022-09-28T13:30:00',
-          },
-        ]}
-        scheduleView
-        taskView
-        template={{
-          milestone(schedule) {
-            return `<span style="color:#fff;background-color: ${schedule.bgColor};">${schedule.title}</span>`;
-          },
-          milestoneTitle() {
-            return 'Milestone';
-          },
-          allday(schedule) {
-            return `${schedule.title}<i class="fa fa-refresh"></i>`;
-          },
-          alldayTitle() {
-            return 'All Day';
-          },
-        }}
-        // theme={myTheme}
-        timezones={[
-          {
-            timezoneOffset: 540,
-            displayLabel: 'GMT+09:00',
-            tooltip: 'Seoul',
-          },
-          {
-            timezoneOffset: -420,
-            displayLabel: 'GMT-08:00',
-            tooltip: 'Los Angeles',
-          },
-        ]}
-        useDetailPopup
-        useCreationPopup
-        view={'month'} // You can also set the `defaultView` option.
-        week={{
-          showTimezoneCollapseButton: true,
-          timezonesCollapsed: true,
-        }}
+        localizer={localizer}
+        events={reservation_day}
+        view={'month'}
+        views={['month']}
+        step={60}
+        style={{ height: 600 }}
       />
       <WritePostButtonWrapper>
         {showWriteButton && (
